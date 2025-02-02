7bc8a576e1a402e1c57e51c3e641ec54d1391072
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2025-02-02 13:15:26 +0900

    Add initial project setup with TypeScript, Mocha, and Chai
    Set up a new TypeScript-based project with default configurations, including Mocha and Chai for testing. Added a basic `main.ts` file and initialized necessary dependencies in `package.json`. Included `tsx`, TypeScript, and other related tools in the dev environment.

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+console.log("hello world")
+
+function add(x:number,y:number):number{
+    return x+y
+}
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+
+describe('formatAwsAccountId', () => {
+    it("'123412341234'の時、'1234-1234-1234'", () => {
+        assert.equal(10, 10);
+    });
+});
